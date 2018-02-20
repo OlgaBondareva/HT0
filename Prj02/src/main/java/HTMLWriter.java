d8f3c6323b5783ed4e63870fef85ee8ddefae632
Olga <vol.bondarava@gmail.com>
2018-02-21 02:02:36 +0300

    Created new classes.

--- Prj02/src/main/java/HTMLWriter.java
+++ Prj02/src/main/java/HTMLWriter.java
@@ -3,23 +3,70 @@ import java.io.IOException;
 import java.util.LinkedList;
 
 public class HTMLWriter {
-    private String outputFilename;
-    private LinkedList<String> filesInfo;
+    private String outputFilename = "filesInfo.html";
+    private LinkedList<Artist> artistList;
 
-    public HTMLWriter(String outputFilename) {
-        this.outputFilename = outputFilename;
-        filesInfo = new LinkedList<String>();
+    /**
+     * Default constructor
+     */
+    public HTMLWriter() {
+        artistList = new LinkedList<>();
     }
 
+    /**
+     * Constructor with one parameter
+     *
+     * @param outputFilename name of the generated output html file
+     */
+    public HTMLWriter(String outputFilename) {
+        this.outputFilename = outputFilename;
+        artistList = new LinkedList<>();
+    }
+
+    /**
+     * @return false when html file didn't create due to error
+     * and true, when file was generate
+     * @throws IOException
+     */
     public boolean createHTML() throws IOException {
         File htmlFile = new File(outputFilename);
+        // Check for file creation
         if (!htmlFile.createNewFile()) {
             System.out.println("An error occurred while creating html file.");
             return false;
         }
         StringBuilder htmlBuilder = new StringBuilder();
         htmlBuilder.append("<!DOCTYPE html>");
+        htmlBuilder.append("\n <html>").append("\n  <head>");
+        htmlBuilder.append("\n   <meta charset=utf-8\">");
+        htmlBuilder.append("\n  </head>");
+        htmlBuilder.append("\n  <body>");
+        for (Artist element : artistList) {
+            htmlBuilder.append("<pre>").append(element.getName()).append("</pre>");
+            /*htmlBuilder.append("<pre>").append(" ").append(element.getAlbums());*/
+            /*htmlBuilder.append(element.getMinutesDuration()).append(" : ").append(element.getSecondsDuration());
+            htmlBuilder.append("(").append(element.getFileLocation()).append(")").append("</pre>");*/
+            for (Artist artist: artistList) {
+                htmlBuilder.append("<pre>").append(artist.getName()).append("</pre>");
+                for (String album: artist.getAlbumsNames()) {
+                    htmlBuilder.append("<pre>").append(" " + album).append("</pre>");
+                    LinkedList<Song> songs = artist.getSongsFromAlbum(album);
+                    for (Song song: songs) {
+                        htmlBuilder.append("<pre>").append(song.getTitle());
+                        htmlBuilder.append(song.getMinutesDuration()).append(":").append(song.getSecondsDuration());
+                        htmlBuilder.append("(").append(song.getFileLocation()).append(")");
+                    }
+                }
+            }
+        }
 
+
+        htmlBuilder.append("\n  </body>");
+        htmlBuilder.append("\n </html>");
         return true;
+    }
+
+    public void add(Artist info) {
+        artistList.add(info);
     }
 }
---
+++ Prj02/src/main/java/Song.java
@@ -0,0 +1,83 @@
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.parser.mp3.Mp3Parser;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Song {
+    private String fileLocation;
+    private String title;
+    private String genre;
+    private String artist;
+    private String album;
+    private double duration;
+    private int minutesDuration;
+    private int secondsDuration;
+
+    public Song() {
+        InputStream input;
+        ContentHandler handler;
+        Metadata metadata = null;
+        Parser parser;
+        ParseContext parseCtx;
+        try {
+            input = new FileInputStream(new File(fileLocation));
+            handler = new DefaultHandler();
+            metadata = new Metadata();
+            parser = new Mp3Parser();
+            parseCtx = new ParseContext();
+            parser.parse(input, handler, metadata, parseCtx);
+            input.close();
+        } catch (IOException | SAXException | TikaException e) {
+            e.printStackTrace();
+        }
+
+        title = metadata.get("title");
+        artist = metadata.get("xmpDM:artist");
+        genre = metadata.get("xmpDM:genre");
+        album = metadata.get("xmpDM:album");
+        duration = Double.parseDouble(metadata.get("xmpDM:duration"));
+        minutesDuration = (int) (duration / 60000);
+        secondsDuration = (int) (duration % 60000) / 1000;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public String getFileLocation() {
+        return fileLocation;
+    }
+
+    public int getMinutesDuration() {
+        return minutesDuration;
+    }
+
+    public int getSecondsDuration() {
+        return secondsDuration;
+    }
+}
